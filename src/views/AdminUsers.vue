f798bc655b6885c68610816616ebd8bf30d47620
zoevds <zoevanderschyff13@gmail.com>
2022-07-15 13:28:17 +0200

    updates to admin pages

--- src/views/AdminUsers.vue
+++ src/views/AdminUsers.vue
@@ -14,6 +14,7 @@
               <th scope="col">Location</th>
               <th scope="col">About</th>
               <th scope="col">Role</th>
+              <th scope="col">Edit</th>
             </tr>
           </thead>
           <tbody>
@@ -36,12 +37,128 @@
                         <i class="fa-solid fa-trash bin"></i>
                       </button>
                     </div>
-                    <i
-                      class="fa-solid fa-pen edit"
-                      onclick="editProperty(${
-                    properties.id
-                });"
-                    ></i>
+                    <div class="col">
+                      <button
+                        type="button"
+                        class="btn"
+                        data-bs-toggle="modal"
+                        data-bs-target="#editmodal"
+                      >
+                        <i class="fa-solid fa-pen edit"></i>
+                      </button>
+                      <!-- Modal 2-->
+                      <div class="modal fade" id="editmodal" tabindex="-1">
+                        <div class="modal-dialog">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h5 class="modal-title" id="editmodalLabel">
+                                Edit users
+                              </h5>
+                              <button
+                                type="button"
+                                class="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                              ></button>
+                            </div>
+                            <div class="modal-body">
+                              <form @submit.prevent="updateBook(book.id)">
+                                <div class="title form-floating">
+                                  <input
+                                    type="text"
+                                    class="form-control"
+                                    id="floatingPassword"
+                                    placeholder="Title"
+                                    v-model="user.email"
+                                  />
+                                  <label for="floatingTitle">email</label>
+                                </div>
+
+                                <div class="description form-floating">
+                                  <input
+                                    type="text"
+                                    class="form-control"
+                                    id="floatingDescription"
+                                    placeholder="Description"
+                                    v-model="user.password"
+                                  />
+                                  <label for="floatingDescription"
+                                    >password</label
+                                  >
+                                </div>
+
+                                <div class="description form-floating">
+                                  <input
+                                    type="text"
+                                    class="form-control"
+                                    id="floatingDescription"
+                                    placeholder="Description"
+                                    v-model="user.username"
+                                  />
+                                  <label for="floatingDescription"
+                                    >username</label
+                                  >
+                                </div>
+
+                                <div class="year form-floating">
+                                  <input
+                                    type="number"
+                                    class="form-control"
+                                    id="floatingYear"
+                                    placeholder="Year"
+                                    v-model="user.avatar"
+                                  />
+                                  <label for="floatingYear">avatar</label>
+                                </div>
+
+                                <div class="pages form-floating">
+                                  <input
+                                    type="number"
+                                    class="form-control"
+                                    id="floatingPages"
+                                    placeholder="Pages"
+                                    v-model="user.location"
+                                  />
+                                  <label for="floatingPages">location</label>
+                                </div>
+
+                                <div class="file form-floating mb-3">
+                                  <input
+                                    type="text"
+                                    class="form-control"
+                                    id="floatingFile"
+                                    placeholder="file url"
+                                    v-model="user.about"
+                                  />
+                                  <label for="floatingInput">about</label>
+                                </div>
+
+                                <div class="file form-floating mb-3">
+                                  <input
+                                    type="text"
+                                    class="form-control"
+                                    id="floatingFile"
+                                    placeholder="file url"
+                                    v-model="user.role"
+                                  />
+                                  <label for="floatingInput">role</label>
+                                </div>
+
+                                <div class="modal-footer">
+                                  <button
+                                    type="button"
+                                    class="btn btn-secondary"
+                                    data-bs-dismiss="modal"
+                                  >
+                                    Close
+                                  </button>
+                                </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </td>
