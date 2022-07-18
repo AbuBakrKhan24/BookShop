b5cbbd3584dee0828a0a9e61f63cf3cf78802ed1
AbuBakrKhan24 <abubakrkhan224@gmail.com>
2022-07-18 02:07:25 +0200

    work from new

--- src/App.vue
+++ src/App.vue
@@ -25,25 +25,8 @@ export default {
 *{
 font-family: 'Varela';
 }
-/* .profileAvatar[data-v-41458b80] {
-    right: 100px;
-    position: absolute;
-    border-radius: 50%;
-} */
 
-div#footer {
-    margin-top: 8%;
-    background-color: rgba(0, 0, 0, 0.367);
-    width: 100%;
-}
-#hello {
-    padding-top: 180px;
-    /* padding-top: 155px; */
-    /* padding-bottom: 8%; */
-    /* height: 100%; */
-    /* height: 92vh; */
-    background-image: linear-gradient( to left bottom, #d16ba5, #c777b9, #ba83ca, #aa8fd8, #9a9ae1, #8aa7ec, #79b3f4, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1 );
-}
+
 
 #app {
   /* height: 100vh; */
